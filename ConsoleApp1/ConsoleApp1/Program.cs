8b2debebfe5389a7d2e0c04bddb06360d3efed98
seung611 <a46070766@gmail.com>
2026-08-27 18:00:44 +0900

    [Feat] Main 기본 설정

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Program  
+{  
+    public static void Main()  
+    {  
+        Console.WriteLine("=== 2조 팀원 소개 ===");  
+        // 여기에 팀원 소개 호출을 한 줄씩 추가합니다
+
+        Console.WriteLine("=== 소개 끝 ===");  
+    }
+
+    // 여기에 팀원 소개 메서드를 하나씩 추가합니다  
+}
