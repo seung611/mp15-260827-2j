0be40a77a7fe2dd8540fe432bef076473b7ddc17
Rocket22 <ldgnl22@gmail.com>
2026-08-27 18:38:51 +0900

    [Feat] IntroRocket22함수 선언과 Main에서 호출함
    #2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,9 +6,14 @@ public class Program
     {  
         Console.WriteLine("=== 2조 팀원 소개 ===");  
         // 여기에 팀원 소개 호출을 한 줄씩 추가합니다
+        IntroRocket22();
 
         Console.WriteLine("=== 소개 끝 ===");  
     }
 
     // 여기에 팀원 소개 메서드를 하나씩 추가합니다  
+    public static void IntroRocket22()
+    {
+        Console.WriteLine("안녕하세요 Rocket22 입니다.");
+    }
 }
