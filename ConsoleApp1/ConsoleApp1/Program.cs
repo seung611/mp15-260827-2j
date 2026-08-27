c48feb0a15f012f83aca170bed8225cd8aa3fdf2
Zebi <einz.yume@gmail.com>
2026-08-27 18:30:59 +0900

    [Feat] add name
    이름 출력 메서드 추가
    본문에 해당 메서드 호출
    Closes #3

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,10 +5,13 @@ public class Program
     public static void Main()  
     {  
         Console.WriteLine("=== 2조 팀원 소개 ===");  
-        // 여기에 팀원 소개 호출을 한 줄씩 추가합니다
+        IntroAsteYume();
 
         Console.WriteLine("=== 소개 끝 ===");  
     }
 
-    // 여기에 팀원 소개 메서드를 하나씩 추가합니다  
+    public static void IntroAsteYume()
+    {
+        Console.WriteLine("김하늘 / 타자가 빠릅니다");
+    } 
 }
